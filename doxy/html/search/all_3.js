839df07f301b1a12d533f906ca151910dee7172d
rudkevich-anton <123441685+rudkevich-anton@users.noreply.github.com>
2023-12-05 13:17:33 +0300

    Add files via upload

---
+++ doxy/html/search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['readme_0',['readme',['../md__c_1_2_users_2kruva_2_downloads_2server-main_2readme.html',1,'']]],
+  ['readme_2emd_1',['readme.md',['../readme_8md.html',1,'']]]
+];
---
+++ doxy/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readme_0',['readme',['../md__c_1_2_users_2kruva_2_downloads_2server-main_2readme.html',1,'']]]
+];
